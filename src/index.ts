1e586cbfe981cda9a95b04639bcfaa2d318c2b91
redstonekasi <kasi@hanneskann.de>
2022-11-05 20:59:44 +0100

    make optional arguments possible

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,13 @@
 type ApplyWithArgument<Stores extends any[]> = (argument: string, index: number, ...stores: Stores) => string | void;
 type ApplyWithoutArgument<Stores extends any[]> = (index: number, ...stores: Stores) => string | void;
 
-interface CommandWithArgument<Stores extends any[]> {
-  hasArgument: true;
-  apply: ApplyWithArgument<Stores>;
-}
+interface Command<Stores extends any[]> {
+  withArg?: ApplyWithArgument<Stores>;
+  withoutArg?: ApplyWithoutArgument<Stores>;
+};
 
-interface CommandWithoutArgument<Stores extends any[]> {
-  hasArgument: false;
-  apply: ApplyWithoutArgument<Stores>;
-}
-
-type Command<Stores extends any[]> = CommandWithArgument<Stores> | CommandWithoutArgument<Stores>;
-type Commands<Stores extends any[]> = {
-  [index: string]: Command<Stores>;
+interface Commands<Stores extends any[]> {
+  [index: string]: Command<Stores>
 };
 
 /**
@@ -37,14 +31,14 @@ export class TextParser<Stores extends any[]> {
   registerCommand(command: string, hasArgument: true, apply: ApplyWithArgument<Stores>): void;
   registerCommand(command: string, hasArgument: false, apply: ApplyWithoutArgument<Stores>): void;
   registerCommand(command: string, hasArgument: boolean, apply: ApplyWithArgument<Stores> | ApplyWithoutArgument<Stores>): void {
-    if (this.commands[command])
+    if (this.commands[command]?.[hasArgument ? "withArg" : "withoutArg"])
       throw Error(`Command for key "${command}" is already assigned`);
 
-      // @ts-expect-error fuck you ts
-      this.commands[command] = {
-        hasArgument,
-        apply,
-      };
+    this.commands[command] ??= {};
+    if (hasArgument)
+      this.commands[command].withArg = apply as ApplyWithArgument<Stores>;
+    else
+      this.commands[command].withoutArg = apply as ApplyWithoutArgument<Stores>;
   }
 
   /**
@@ -60,17 +54,19 @@ export class TextParser<Stores extends any[]> {
     while (pos !== input.length) {
       let skip = input.indexOf("\\", pos);
       if (skip === -1) break;
-  
+
       result += input.substring(pos, skip);
       pos = skip + 1;
-  
+
       let char = input[skip + 1]
       let command = this.commands[char];
+
       if (command) {
         let replacement: string | void;
-  
-        if (command.hasArgument) {
-          let start = input.indexOf("[", skip);
+
+        let start = input.indexOf("[", skip);
+
+        if (start === skip + 2 && command.withArg) {
           let end = input.indexOf("]", start);
 
           const offset = input.substring(start, end).split("[").length - 2;
@@ -78,18 +74,22 @@ export class TextParser<Stores extends any[]> {
             end = input.indexOf("]", end + 1);
           }
 
-          if (start !== skip + 2 || end === -1) {
+          if (end === -1) {
             // result += input[skip];
             // this is actually just supposed to be a warning
             throw Error(`Invalid argument format for command "${char}"`);
           }
-  
+
           let argument = input.substring(start + 1, end);
           pos = end + 1;
-          replacement = command.apply(argument, result.length, ...stores);
-        } else {
+          replacement = command.withArg(argument, result.length, ...stores);
+        } else if (command.withoutArg) {
           pos += 1;
-          replacement = command.apply(result.length, ...stores);
+          replacement = command.withoutArg(result.length, ...stores);
+        } else {
+          // this should only be triggered if a command can only be executed
+          // with an argument but doesn't get passed one
+          throw new Error(`Expected an argument for command "${char}"`);
         }
   
         if (replacement) {
--- tests/index.ts
+++ tests/index.ts
@@ -2,16 +2,29 @@ import { describe, expect } from "manten";
 import { TextParser } from "../src";
 
 describe("TextParser", ({ test }) => {
-  test("Arguments", () => {
+  test("Argument", () => {
     const parser = new TextParser();
     parser.registerCommand("t", true, (arg) => arg.toUpperCase());
     expect(parser.parse("Hello \\t[world]")).toBe("Hello WORLD");
   });
 
-  test("No arguments", () => {
+  test("No argument", () => {
     const parser = new TextParser();
     parser.registerCommand("t", false, () => "world");
     expect(parser.parse("Hello \\t")).toBe("Hello world");
+  });
+
+  test("Expected argument", () => {
+    const parser = new TextParser();
+    parser.registerCommand("t", true, () => "world");
+    expect(() => parser.parse("Hello \\tworld")).toThrow();
+  })
+
+  test("Optional argument", () => {
+    const parser = new TextParser();
+    parser.registerCommand("t", false, () => "Hello");
+    parser.registerCommand("t", true, (arg) => arg.toUpperCase());
+    expect(parser.parse("\\t \\t[world]")).toBe("Hello WORLD");
   });
 
   test("Invalid format", () => {
@@ -30,6 +43,8 @@ describe("TextParser", ({ test }) => {
   test("Can't overwrite existing command", () => {
     const parser = new TextParser();
     parser.registerCommand("t", false, () => {});
+    parser.registerCommand("t", true, () => {});
     expect(() => parser.registerCommand("t", false, () => {})).toThrow();
+    expect(() => parser.registerCommand("t", true, () => {})).toThrow();
   });
 });
